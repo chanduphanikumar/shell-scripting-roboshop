a734098b30b3a3f4daaa5c5dceb5eb66ccbc93c7
chanduphanikumar <chanduphanikumar026@gmail.com>
2022-01-03 22:22:32 +0530

    update roboshop commands

--- Frontend.sh
+++ Frontend.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
 
-# source is nothing but import , like export command
-source components/common.sh
+# yum install nginx -y
+# systemctl enable nginx
+# systemctl start nginx
 
-yum install nginx -y &>>${LOG_FILE}
-STAT_CHECK $? "Nginx Installation"
+# curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
-DOWNLOAD frontend
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-master static README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-rm -rf /usr/share/nginx/html/*
-STAT_CHECK $? "Remove old HTML Pages"
-
-cd  /tmp/frontend-main/static/ && cp -r * /usr/share/nginx/html/
-STAT_CHECK $? "Copying Frontend Content"
-
-cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-STAT_CHECK $? "Copy Nginx Config File"
-
-for component in catalogue cart user shipping payment ; do
-  sed -i -e "/${component}/ s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
-done
-STAT_CHECK $? "Update Nginx Config File"
-
-systemctl enable nginx &>>${LOG_FILE} && systemctl restart nginx &>>${LOG_FILE}
-STAT_CHECK $? "Restart Nginx"
-
-
-
-
-
-
-
+# systemctl restart nginx
