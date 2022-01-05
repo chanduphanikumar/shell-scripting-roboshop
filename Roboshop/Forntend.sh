859388043a300a54a1eda88654c412810a6fdd9c
chanduphanikumar <chanduphanikumar026@gmail.com>
2022-01-05 23:12:41 +0530

    Sampul.sh
    robo shop

--- Roboshop/Forntend.sh
+++ Roboshop/Forntend.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
+echo
 
-# yum install nginx -y
-# systemctl enable nginx
-# systemctl start nginx
-Let's download the HTML content that serves the RoboSHop Project UI and deploy under the Nginx path.
-
-# curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+yum install nginx -y
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 Deploy in Nginx Default Location.
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-master static README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-master static README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
-Finally restart the service once to effect the changes.
+systemctl enable nginx
+systemctl restart nginx
 
-# systemctl restart nginx
 
