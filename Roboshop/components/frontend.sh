6eeefe9d6e504fb39234ad55c3c0154ac8792fd7
chanduphanikumar <chanduphanikumar026@gmail.com>
2022-01-07 21:50:49 +0530

    Frontend created

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -9,6 +9,11 @@ fi
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 Deploy in Nginx Default Location.
+if [ $? -ne 0 ] ; than
+echo "download frontend failed"
+exit 1
+fi
+
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
