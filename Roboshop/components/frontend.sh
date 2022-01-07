a092a1edd22d919276f25b5a9ddc66687417a90b
chanduphanikumar <chanduphanikumar026@gmail.com>
2022-01-07 22:24:07 +0530

    Frontend created   f

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -1,20 +1,21 @@
 #!\bin/bash
 
 
-yum install nginx -y
+yum install ninx -y
 if [ $? -ne 0]; than
 echo "Nginx install failed"
 exit 1
 fi
 
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zi"
 Deploy in Nginx Default Location.
 if [ $? -ne 0 ] ; than
 echo "download frontend failed"
 exit 1
 fi
 
-cd /usr/share/nginx/html
+echo cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
 mv frontend-main/* .
