ca2580c45aaf3edb770d561a0461a66de0391d32
chanduphanikumar <chanduphanikumar026@gmail.com>
2022-01-10 21:56:29 +0530

    frotend

---
+++ .idea/roboshop/frontend.sh
@@ -0,0 +1,16 @@
+#!\bin\bash
+
+
+
+
+
+
+
+
+USER UID=$(id -u)
+if [ ${USER_UID} -ne 0 ]; than
+  echo -e "/e[1,31myou shouid be a root user to perform this script\e[0m"
+  exit
+fi
+
+yum install nginx -y
