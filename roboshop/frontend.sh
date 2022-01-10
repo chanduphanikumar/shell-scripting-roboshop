5fe87f5d97a8210dd1ba604eb3c7ed267fa85b84
chanduphanikumar <chanduphanikumar026@gmail.com>
2022-01-10 22:06:27 +0530

    added new

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -4,7 +4,7 @@
 
 USER_UID=$(id -u)
 if [ ${USER_UID} -ne 0 ]; than
-  echo -e "/e[1,31myou shouid be a root user to perform this script\e[0m"
+  echo -e "/e[1;31myou shouid be a root user to perform this script\e[0m"
   exit
 fi
 
